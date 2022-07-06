27b5f9c64e34189a8638ba2bfb6df33c4fd56334
Bernd Ruecker <bernd.ruecker@camunda.com>
2022-07-06 09:55:04 +0200

    Added hint on job error handling

--- connector-runtime-job-worker/src/main/java/io/camunda/connectors/ConnectorJobHandler.java
+++ connector-runtime-job-worker/src/main/java/io/camunda/connectors/ConnectorJobHandler.java
@@ -20,6 +20,9 @@ public class ConnectorJobHandler implements JobHandler {
   @Override
   public void handle(JobClient client, ActivatedJob job) {
     LOGGER.debug("Received job {}", job.getKey());
+    // TODO: I think of using the normal Spring execution handling including failure handling
+    // around retries, backoff, etc...
+    // See https://github.com/camunda-community-hub/spring-zeebe/blob/075884c7b34ed18355bfdeb00233fa4b9f98d2fd/client/spring-zeebe/src/main/java/io/camunda/zeebe/spring/client/jobhandling/CommandWrapper.java#L13
 
     try {
 
@@ -30,6 +33,7 @@ public class ConnectorJobHandler implements JobHandler {
     } catch (Exception error) {
 
       LOGGER.debug("Failed to process job {}", job.getKey(), error);
+      // Why no retries? 
       client.newFailCommand(job).retries(0).errorMessage(error.getMessage()).send().join();
 
     }
