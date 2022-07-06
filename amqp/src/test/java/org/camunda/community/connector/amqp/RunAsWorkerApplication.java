1d5609ca5f8fcbce963884064c9c24e1ecc96552
Bernd Ruecker <bernd.ruecker@camunda.com>
2022-07-06 11:54:01 +0200

    Switched dependency to runtime to "test" scope

--- amqp/src/test/java/org/camunda/community/connector/amqp/RunAsWorkerApplication.java
+++ amqp/src/test/java/org/camunda/community/connector/amqp/RunAsWorkerApplication.java
@@ -1,10 +1,10 @@
 package org.camunda.community.connector.amqp;
 
+
 import io.camunda.connectors.ConnectorRuntimeJobWorkerApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
-//@Configuration(class=ConnectorRuntimeJobWorkerApplication.class)
 public class RunAsWorkerApplication extends ConnectorRuntimeJobWorkerApplication {
 }
